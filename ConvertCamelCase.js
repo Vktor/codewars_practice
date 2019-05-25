43d1dfe58d34795f21dcd551736fff29f23190e1
Victor <victor.carranz@hotmail.com>
2019-05-25 13:37:24 -0600

    convert string to camel case

---
+++ ConvertCamelCase.js
@@ -0,0 +1,33 @@
+// INSTRUCCION ////////////////////////////
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+The first word within the output should be capitalized only if the original word was capitalized.
+*/
+
+//Examples /////////////////////////////////////////////////////////////
+/* 
+toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+
+toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+
+Sample Tests://///////////////////////////////////////////////
+1
+Test.assertEquals(toCamelCase(''), '', "An empty string was provided but not returned")
+2
+Test.assertEquals(toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+3
+Test.assertEquals(toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
+4
+Test.assertEquals(toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value")
+5
+*/
+
+// MY SOLUTION /////////////////////////////////////////////////////////////////////////////////////
+
+function toCamelCase(str){
+
+}
+
+
+
+
